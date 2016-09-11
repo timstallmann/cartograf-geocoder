8ebd204cba48e42ba8d0773cf105e012603583f6
Tim Stallmann <tim@stallmann.mayfirst.org>
2016-09-11 18:58:45 -0400

    Filter MapQuest results by granularity

--- src/MapQuestNominatim.php
+++ src/MapQuestNominatim.php
@@ -13,6 +13,23 @@ use Cartograf\GeoCoder\Geocoder;
 class MapQuestNominatim extends GeoCoder {
   protected $apiKey;
 
+  // Ranked array of the possibly values of the first 2 characters of
+  // geocodeQualityCode from result, ordered from most precise to least.
+  protected $granularityRanking;
+
+  // Minimum precision which will be accepted.
+  protected $minGranularity;
+
+  public function __construct($ch) {
+    parent::__construct($ch);
+    $this->granularityRanking = array_flip(array('P1', 'L1', 'I1', 'B1', 'B2', 'B3', 'Z4', 'Z3', 'Z2', 'A6', 'Z1', 'A5', 'A4', 'A3', 'A2', 'A1'));
+    $this->minGranularity = 'A6';
+  }
+
+  public function setMinGranularity($min_granularity) {
+    $this->minGranularity = $min_granularity;
+  }
+
   public function getBaseUrl() {
     if ($this->apiKey) {
       return 'http://open.mapquestapi.com/geocoding/v1/address?key=' . $this->apiKey;
@@ -57,23 +74,33 @@ class MapQuestNominatim extends GeoCoder {
   }
 
   protected function getLatLngFromResult(\stdClass $result) {
-    $return = new LatLng;
-    if ($result && property_exists($result, "results") && is_array($result->results) && property_exists($result->results[0], "locations") && is_array($result->results[0]->locations)) {
-      if (count($result->results[0]->locations) > 0) {
-        $return->latitude = $result->results[0]->locations[0]->latLng->lat;
-        $return->longitude = $result->results[0]->locations[0]->latLng->lng;
-      } //valid response & locations
-      else {
-        return FALSE;
-      } //valid response, no locations
+    $return = FALSE;
+    if ($result) {
+      if (property_exists($result,
+          "results") && is_array($result->results) && property_exists($result->results[0],
+          "locations") && is_array($result->results[0]->locations)
+      ) {
+        if (count($result->results[0]->locations) > 0) {
+          foreach ($result->results[0]->locations as $location) {
+            if ($this->granularityRanking[substr($location->geocodeQualityCode,
+                0,
+                2)] <= $this->granularityRanking[$this->minGranularity]
+            ) {
+              $return = new LatLng;
+              $return->latitude = $result->results[0]->locations[0]->latLng->lat;
+              $return->longitude = $result->results[0]->locations[0]->latLng->lng;
+              break;
+            }
+          }
+        }
+      }
+      elseif (!empty($result->lat) && !empty($result->lng)) {
+        $return = new LatLng;
+        $return->latitude = $result->lat;
+        $return->longitude = $result->lng;
+      }
     }
-    elseif ($result) {
-      $return->latitude = $result->lat;
-      $return->longitude = $result->lng;
-    }
-    else {
-      return FALSE;
-    }
+
     return $return;
   }
 }
